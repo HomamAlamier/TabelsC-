b3af61e1853a6e8921c9372bcdb40fc1dfed4133
HomamAlamier <homamassr2@gmail.com>
2018-12-18 20:48:22 +0200

    Update Tables.cs
    update 0.05:
    -add CreateFindWindow for creating a search windows, and it's have 2 types:
    1-Find Window : find a word in the table and highlight the cell with a color
    2=FindAndReplace Window : find a word and replace it with another (if there more then one word it will replace all)
    usage : 
    CreateFindWindow(int WindowsMode)
    Windows Mode Can be found on the main class
    Tables.WindowsMode_FindOnly
    Tables.WindowsMode_FindAndReplace
    
    change log : 
    add new function above
    make all functions not work if there is no table created
    some edits and bug fix's
    
    thats all for now 👍

--- Tables.cs
+++ Tables.cs
@@ -9,11 +9,8 @@ namespace WindowsFormsApp4
 { 
     class Tables
     {
-        public struct pos
-        {
-            public int cellX;
-            public int cellY;
-        }
+        public int WindowsMode_FindAndReplace = 10020;
+        public int WindowsMode_FindOnly = 10300;
         public struct mytextbox
         {
             public Point location;
@@ -29,31 +26,41 @@ namespace WindowsFormsApp4
             public int width;
             public mytextbox[,] points;
             public System.Drawing.Point point;
- 
+            public int TableX;
+            public int TableY;
             public Form1 InForm;
+            
         }
+        public static  bool IsCreated = false;
         static bool clicked = false;
        public static mystract Table ;
         public static int TableCount = 0;
-        public void CreateTable(int Height,int Width,Form1 frm,int mode,string[] titles = null, int x = 0, int y = 0)
+        public void CreateTable(int Height,int Width,Form1 frm,int x , int y )
         {
+            if (IsCreated == false)
+                IsCreated = true;
+            else
+                return;
             Table.DataBase = new string[Height + 1, Width +1];
             Table.height = Height;
             Table.width = Width;
             Table.point = new System.Drawing.Point(x, y);
             Table.InForm = frm;
+            Table.TableX = x;
+            Table.TableY = y;
         }
-        public void DrawTable(int width,int height,int x,int y)
+        public void DrawTable(int width,int height)
         {
+            if (IsCreated == false) return;
+            int x = Table.TableX;
+            int y = Table.TableY;
             Table.points = new mytextbox[ Table.height, Table.width];
             int w = (width / (Table.width)) ;
             int h = (height / (Table.height)) ;
-
             int hh = h;
             h = h * Table.height;
             int wr = (width % (Table.width));
             Graphics g;
-            
             g = Table.InForm.CreateGraphics();
             g.Clear(Color.White);
             Pen myPen = new Pen(Color.Black);
@@ -95,13 +102,12 @@ namespace WindowsFormsApp4
                         if (tT.Length > max) tT=tT.Substring(0, max) + "...";
                         g.DrawString(tT, fnt, br,new Point(Table.points[i, j].location.X , (Table.points[i, j].location.Y- Table.points[i, j].height) + 2));
                     }
-
                 }
             }
-
         }
         public void ClickOnTable(int x,int y)
         {
+            if (IsCreated == false) return;
             if (clicked==false )
             {
                 for (int i = 0; i < Table.height ; i++)
@@ -143,7 +149,7 @@ namespace WindowsFormsApp4
         {
             TextBox tx = (TextBox)sender;
             tx.Dispose();
-            DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight, Table.points[0, 1].location.X, 25);
+            DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight);
         }
         private void textClick_KeyPress(object sender,KeyPressEventArgs e)
         {
@@ -151,7 +157,7 @@ namespace WindowsFormsApp4
             {
                 TextBox tx = (TextBox)sender;
                 tx.Dispose();
-                DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight, Table.points[0, 1].location.X, 25);
+                DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight);
             }
             }
 
@@ -170,8 +176,9 @@ namespace WindowsFormsApp4
         {
             return Table.points[0,0].allWidth;
         }
-        public void findCell(string input,int X=0,int Y =0 )
+        public void findCell(string input)
         {
+            if (IsCreated == false) return;
             int i=0, j=0;
             for ( i = 0; i <= Table.height ; i++)
             {
@@ -179,14 +186,21 @@ namespace WindowsFormsApp4
                 {
                     if (Table.DataBase[i, j] != null && Table.DataBase[i,j].IndexOf(input) >=0 )
                     {
-                        TextBox tx = new TextBox();
-                        
+                        Graphics g = Table.InForm.CreateGraphics();
+                        int x = Table.points[i, j].location.X + 1;
+                        int y = Table.points[i, j].location.Y - Table.points[i, j].height;
+                        y++;
+                        Rectangle rec = new Rectangle(new Point(x,y), new Size(Table.points[i,j].width-1, Table.points[i,j].height-1));
+                        Region r = new Region(rec);
+                        g.FillRegion(new SolidBrush(Color.LightCyan), r);
+                        g.DrawString(Table.DataBase[i,j], new Font("Arial", 10), new SolidBrush(Color.Black), new Point(x,y));
                     }
                 }
             }
         }
         public void findAndreplace(string input,string input2)
         {
+            if (IsCreated == false) return;
             int i = 0, j = 0;
             for (i = 0; i <= Table.height; i++)
             {
@@ -194,10 +208,11 @@ namespace WindowsFormsApp4
                 {
                     if (Table.DataBase[i, j] != null && Table.DataBase[i, j].IndexOf(input) >= 0)
                     {
-
+                        Table.DataBase[i, j] = input2;
                     }
                 }
             }
+            DrawTable(Table.points[0, 0].allWidth, Table.points[0, 0].allHeight);
         }
         public int GetHeight()
         {
@@ -213,6 +228,7 @@ namespace WindowsFormsApp4
         }
         public void saveToFile(string filename)
         {
+            if (IsCreated == false) return;
             System.IO.FileStream fs = System.IO.File.Open(filename, System.IO.FileMode.OpenOrCreate);
             string x = Table.height + ":" + Table.width + "/" ;
             for (int i = 0; i <= Table.height ; i++)
@@ -247,7 +263,7 @@ namespace WindowsFormsApp4
             int f2 = u.Length;
             int h = int.Parse( u.Substring(0, f1));
             int w = int.Parse(u.Substring(f1+1 , f2-(f1+1)));
-            CreateTable(h, w, frm, 1,null,1,25);
+            CreateTable(h, w, frm, 1,25);
             int pointer =0 ;
             int oldpointer=0;
             for (int i =0; i <= h; i++)
@@ -260,20 +276,82 @@ namespace WindowsFormsApp4
                     int co = int.Parse(da.Substring(oldpointer + 1, pointer - (oldpointer + 1)));
                     string dat = da.Substring(pointer + 1, co);
                     if (i == 0)
-                        addData(dat, i, j, false );
+                        Table.DataBase[i, j] = dat;
                     else
-                        addData(dat, i, j);
+                        Table.DataBase[i, j] = dat;
                     pointer += co;
                     oldpointer = pointer;
                 }
             }
-
-            
-
+            DrawTable(Table.InForm.Width - 20, Table.InForm.Height - 1);
         }
-        private void addData(string input,int x,int y,bool enabl = true )
+
+        public void CreateFindWindow(int WindowsMode)
         {
-            Table.DataBase[x, y] = input;
-        }
+            if (IsCreated == false) return;
+            System.Drawing.Size size =new Size();
+            if (WindowsMode == WindowsMode_FindOnly)
+                size = new System.Drawing.Size(200, 80);
+            else
+                size = new System.Drawing.Size(200, 130);
+            Form inputBox = new Form();
+            inputBox.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            inputBox.ClientSize = size;
+            if (WindowsMode == WindowsMode_FindOnly)
+                inputBox.Text = "Find";
+            else
+                inputBox.Text = "Find and replace";
+            Label lbl = new Label();
+            lbl.Text = "Enter a search key word :";
+            lbl.Location = new Point(5, 5);
+            lbl.Height = 20;
+            inputBox.Controls.Add(lbl);
+            System.Windows.Forms.TextBox textBox = new TextBox();
+            textBox.Size = new System.Drawing.Size(size.Width - 10 , 23);
+            textBox.Location = new System.Drawing.Point(5, 25);
+            textBox.Text = "";
+            inputBox.Controls.Add(textBox);
+            Button okButton = new Button();
+            okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            okButton.Name = "okButton";
+            okButton.Size = new System.Drawing.Size(75, 23);
+            okButton.Text = "&OK";
+            if (WindowsMode == WindowsMode_FindOnly)
+                okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 49);
+            else
+                okButton.Location = new System.Drawing.Point(size.Width - 80 - 80, 99);
+            Button cancelButton = new Button();
+            cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new System.Drawing.Size(75, 23);
+            cancelButton.Text = "&Cancel";
+            if (WindowsMode == WindowsMode_FindOnly)
+                cancelButton.Location = new System.Drawing.Point(size.Width - 80, 49);
+            else
+                cancelButton.Location = new System.Drawing.Point(size.Width - 80, 99);
+            TextBox text2 = new TextBox();
+            if (WindowsMode == WindowsMode_FindAndReplace)
+            {
+                Label lbl2 = new Label();
+                lbl2.Text = "Enter a replace key word :";
+                lbl2.Location = new Point(5, 48);
+                lbl2.Height = 20;
+                inputBox.Controls.Add(lbl2);
+                text2.Width = textBox.Width;
+                text2.Location = new Point(5, 68);
+                inputBox.Controls.Add(text2);
+            }
+            inputBox.MaximizeBox = false;
+            inputBox.MinimizeBox = false;
+            inputBox.Controls.Add(okButton);
+            inputBox.Controls.Add(cancelButton);
+            inputBox.AcceptButton = okButton;
+            inputBox.CancelButton = cancelButton;
+            inputBox.ShowDialog();
+            if (WindowsMode == WindowsMode_FindOnly)
+                findCell(textBox.Text);
+            else
+                findAndreplace(textBox.Text, text2.Text);
+        }  
     }
 }

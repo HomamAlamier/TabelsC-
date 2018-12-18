4781b9ad900cb8a2cd3708e2914fb0005015ea34
HomamAlamier <homamassr2@gmail.com>
2018-12-18 18:39:06 +0200

    Update Tables.cs
    update 0.01:
    -very light and speed because i go with drawings :)
    -not full functional because some functions i didn't continue writing *_*

--- Tables.cs
+++ Tables.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Drawing;
 namespace WindowsFormsApp4
 { 
     class Tables
@@ -14,152 +14,150 @@ namespace WindowsFormsApp4
             public int cellX;
             public int cellY;
         }
+        public struct mytextbox
+        {
+            public Point location;
+            public int width;
+            public int height;
+            public int allHeight;
+            public int allWidth;
+        }
         public struct mystract
         {
-           public TextBox[,] TB;
            public string[,] DataBase;
-            public pos[] TBPos;
             public int height;
             public int width;
+            public mytextbox[,] points;
             public System.Drawing.Point point;
-            public int TBwidth;
+ 
+            public Form1 InForm;
         }
-       public static mystract[] Table ;
+        static bool clicked = false;
+       public static mystract Table ;
         public static int TableCount = 0;
         public void CreateTable(int Height,int Width,Form1 frm,int mode,string[] titles = null, int x = 0, int y = 0)
         {
-            if (TableCount > 0)
+            Table.DataBase = new string[Height + 1, Width +1];
+            Table.height = Height;
+            Table.width = Width;
+            Table.point = new System.Drawing.Point(x, y);
+            Table.InForm = frm;
+        }
+        public void DrawTable(int width,int height,int x,int y)
+        {
+            Table.points = new mytextbox[ Table.height, Table.width];
+            int w = (width / (Table.width)) ;
+            int h = (height / (Table.height)) ;
+
+            int hh = h;
+            h = h * Table.height;
+            int wr = (width % (Table.width));
+            Graphics g;
+            
+            g = Table.InForm.CreateGraphics();
+            g.Clear(Color.White);
+            Pen myPen = new Pen(Color.Black);
+            myPen.Width = 1;
+            g.DrawLine(myPen, x, y, x + width, y);
+            g.DrawLine(myPen, x, y, x , y+h);
+            g.DrawLine(myPen, x, y+h , x+width , y + h);
+            g.DrawLine(myPen, x + width, y, x + width, y + h);
+            int x2=0, y2;
+            for (int i = 0; i < Table.height; i++)
             {
-                TableCount++;
-                Table = SwapArrays_mystract(TableCount, Table);
-            }
-            else
-            {
-                Table = new mystract[1];
-               
-            }
-            Table[TableCount].TB = new TextBox[Height+1, Width+1];
-            Table[TableCount].DataBase = new string[Height + 1, Width +1];
-            Table[TableCount].TBPos = new pos[Height + 1+Width ];
-            Table[TableCount].height = Height;
-            Table[TableCount].width = Width;
-            Table[TableCount].point = new System.Drawing.Point(x, y);
-            Table[TableCount].TBwidth = 100;
-        int textboxcount = 0;
-            // int x = 0, y = 0;
-            int oldx = x;
-        for (int i = 0; i <= Height; i++)
-         {
-                for (int j = 0; j <= Width ; j++)
+                for (int j = 0; j < Table.width; j++)
                 {
-                    TextBox tx = new TextBox();
-                    // Default Parameters
-                    tx.TextAlign = HorizontalAlignment.Center;
-                    tx.Location = new System.Drawing.Point(x, y);
-                    tx.Height = 20;
-                    tx.BorderStyle = BorderStyle.FixedSingle;
-                    tx.Name = Convert.ToString(i);
-                    tx.TabIndex = j;
-                    tx.TextChanged += new EventHandler(TB_TextChanged);
-                    // Spesific Parameters
-                    if (j > 0)
-                        tx.Width = 100;
-                    else
+                    Table.points[i, j].allWidth = width;
+                    Table.points[i, j].allHeight = height;
+                    //TextBox tx = Table.TB[0, j].Tag as TextBox;
+                    Table.points[i, j].location = new Point(x2, 0);
+                    x2 = w * j;
+                    g.DrawLine(myPen, x2, y, x2, y + h);
+                    Table.points[i, j].width = w;
+                    y2 = hh * i;
+                    y2 += y;
+                    g.DrawLine(myPen, x, y2, x + width, y2);
+                    Table.points[i,j].location = new Point(Table.points[i,j].location.X, y2);
+                    Table.points[i,j].height = hh;
+                }
+            }
+            for (int i = 0; i < Table.height ; i++)
+            {
+                for (int j = 0; j < Table.width ; j++)
+                {
+
+                if (Table.DataBase[i,j] != null)
                     {
-                        tx.Width = 30;
-                        tx.Enabled = false;
-                        if (textboxcount >0) tx.Text = Convert.ToString( textboxcount);
+                        string tT= Table.DataBase[i, j];
+                        Font fnt = new Font("Arial",10);
+                        SolidBrush br = new SolidBrush(Color.Black) ;
+                        int max = Table.points[i, j].width / 10;
+                        if (tT.Length > max) tT=tT.Substring(0, max) + "...";
+                        g.DrawString(tT, fnt, br,new Point(Table.points[i, j].location.X , (Table.points[i, j].location.Y- Table.points[i, j].height) + 2));
                     }
-                                
-                if (textboxcount == 0 && mode ==1)
-                 {
-                        tx.Enabled = false;
-                        if (titles != null && j <= titles.Length && mode == 1 && j >0) tx.Text = titles[j-1];
-                 } 
-                 // return Parameters
-                 x += tx.Width-1;
-                 frm.Controls.Add(tx);
-                 Table[TableCount].TB[i, j] = new TextBox();
-                 Table[TableCount].TB[i, j].Tag = tx;
 
                 }
-                    textboxcount++;
-                    y += 19;
-                    x = oldx;
-                if (titles != null)
-                {
-                    int z;
-                    if (Width < titles.Length)
-                        z = Width;
-                    else
-                        z = titles.Length;
-                    for (int f = 0; f < z ; f++)
-                    {
-                        Table[TableCount].DataBase[0, f] = titles[f];
-                    }
-                }
-                }
+            }
 
-            TableCount++;
         }
-        public void ResizeTable( int newWidth,int newY=0, int newHeight = 0, int tableIndex = 0)
+        public void ClickOnTable(int x,int y)
         {
-            int o = TableCount;
-            int w = (newWidth / (Table[tableIndex].width))-4;
-            int x = (Table[tableIndex].point.X );
-            int y;
-            if (newY == 0)
-                y = Table[tableIndex].point.Y;
-            else
-                y = newY;
-            Table[tableIndex].TBwidth = w;
-            for (int i = 0; i <= Table[tableIndex].height ; i++)
+            if (clicked==false )
             {
-                x = Table[tableIndex].point.X+29;
-                for (int j = 0; j <= Table[tableIndex].width ; j++)
+                for (int i = 0; i < Table.height ; i++)
                 {
-                    TextBox tx = Table[tableIndex].TB[i, j].Tag as TextBox;
-                    if (j > 0)
+                    for (int j = 1; j < Table.width; j++)
                     {
-                        tx.Width = w;
-                        tx.Location = new System.Drawing.Point(x, y);
-                        x += tx.Width - 1;
+                        if (Table.points[i,j].location.X <= x && (Table.points[i,j].location.X + Table.points[i,j].width) >= x && (Table.points[i,j].location.Y) >= y)
+                        {
+                           //params
+                            TextBox tx = new TextBox();
+                            tx.Height = Table.points[i,j].height + 1;
+                            tx.Width = Table.points[i,j].width;
+                            tx.Location = new Point(Table.points[i, j].location.X + 1, Table.points[i, j].location.Y - Table.points[i, j].height);
+                            tx.Multiline = true;
+                            tx.Leave += new EventHandler(textVlick_Leave);
+                            tx.KeyPress += new KeyPressEventHandler(textClick_KeyPress);
+                            tx.TextChanged += new EventHandler(textClick_TextChanged);
+                            tx.TabIndex = j;
+                            tx.Name = i.ToString();
+                            tx.BackColor = Color.LawnGreen;
+                            tx.Text = Table.DataBase[i, j];
+                            // add
+                            Table.InForm.Controls.Add(tx); 
+                            tx.TextAlign = HorizontalAlignment.Center;
+                            tx.Focus();
+                            return;
+                        }
                     }
-                    else
-                        tx.Location = new System.Drawing.Point(tx.Location.X, y);
                 }
-                y += 19;
             }
         }
-        public void ResizeColomun(int newWidth,int colNum, int tableIndex = 0)
+        private void textClick_TextChanged(object sender, EventArgs e)
         {
-            int o = TableCount;
-            int w = newWidth ;
-            TextBox txx = Table[tableIndex].TB[tableIndex, colNum].Tag as TextBox;
-            int x = (txx.Location.X);
-             int y = txx.Location.Y;
-            for (int i = 0; i <= Table[tableIndex].height; i++)
+            TextBox tx = (TextBox)sender;
+            int i = int.Parse(tx.Name);
+            Table.DataBase[i, tx.TabIndex] = tx.Text;
+        }
+        private void textVlick_Leave(object sender,EventArgs e)
+        {
+            TextBox tx = (TextBox)sender;
+            tx.Dispose();
+            DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight, Table.points[0, 1].location.X, 25);
+        }
+        private void textClick_KeyPress(object sender,KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
             {
-                int xx = (x + newWidth ) - 1;
-                for (int j = 0; j <= Table[tableIndex].width; j++)
-                {
-                    TextBox tx = Table[tableIndex].TB[i, j].Tag as TextBox;
-                    if (colNum == j)
-                        tx.Width = w;
-                    if ( j > colNum)
-                    { 
-                        tx.Location = new System.Drawing.Point(xx, tx.Location.Y );
-                            xx += tx.Width - 1;
-                    }
-                }
+                TextBox tx = (TextBox)sender;
+                tx.Dispose();
+                DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight, Table.points[0, 1].location.X, 25);
             }
-        }
-        private mystract[] SwapArrays_mystract(int newMax,mystract[] old)
+            }
+
+        public void ResizeColomun(int newWidth,int colNum)
         {
-            mystract[] temp = new mystract[newMax];
-            temp = old;
-            old = new mystract[newMax] ;
-            return temp;
+
         }
         private string[,] SwapArrays_Database(int newHeight,int newWidth, string[,] old)
         {
@@ -168,81 +166,62 @@ namespace WindowsFormsApp4
             old = new string[newHeight,newWidth ];
             return temp;
         }
-        public int GetWidth(int tableIndex = 0)
+        public int GetWidth()
         {
-            int temp=0;
-            for (int i = 0; i <= Table[tableIndex].width ; i++)
-            {
-                TextBox tx = Table[tableIndex].TB[0, i].Tag as TextBox;
-                temp += tx.Width-1;
-            }
-            return temp;
+            return Table.points[0,0].allWidth;
         }
-        public void findCell(string input,int X=0,int Y =0, int tableIndex = 0)
+        public void findCell(string input,int X=0,int Y =0 )
         {
             int i=0, j=0;
-            for ( i = 0; i <= Table[tableIndex].height ; i++)
+            for ( i = 0; i <= Table.height ; i++)
             {
-                for (j = 0; j < Table[tableIndex].width ; j++)
+                for (j = 0; j < Table.width ; j++)
                 {
-                    if (Table[tableIndex].DataBase[i, j] != null && Table[tableIndex].DataBase[i,j].IndexOf(input) >=0 )
+                    if (Table.DataBase[i, j] != null && Table.DataBase[i,j].IndexOf(input) >=0 )
                     {
-                        TextBox tx = Table[tableIndex].TB[i, j].Tag as TextBox;
-                        X = i;
-                        Y = j;
-                        tx.BackColor = System.Drawing.Color.LightCyan;
+                        TextBox tx = new TextBox();
+                        
                     }
                 }
             }
         }
-        public void findAndreplace(string input,string input2,int tableIndex =0)
+        public void findAndreplace(string input,string input2)
         {
             int i = 0, j = 0;
-            for (i = 0; i <= Table[tableIndex].height; i++)
+            for (i = 0; i <= Table.height; i++)
             {
-                for (j = 0; j < Table[tableIndex].width; j++)
+                for (j = 0; j < Table.width; j++)
                 {
-                    if (Table[tableIndex].DataBase[i, j] != null && Table[tableIndex].DataBase[i, j].IndexOf(input) >= 0)
+                    if (Table.DataBase[i, j] != null && Table.DataBase[i, j].IndexOf(input) >= 0)
                     {
-                        TextBox tx = Table[tableIndex].TB[i, j].Tag as TextBox;
-                        tx.Text = input2;
+
                     }
                 }
             }
         }
-        public int GetHeight(int tableIndex = 0)
+        public int GetHeight()
         {
-            return (19 * Table[tableIndex].height) + 19;
+            return (19 * Table.height) + 19;
         }
-        public int GetX(int tableIndex = 0)
+        public int GetX()
         {
-            return Table[tableIndex].point.X;
+            return Table.point.X;
         }
-        public int GetY(int tableIndex = 0)
+        public int GetY()
         {
-            return Table[tableIndex].point.Y;
+            return Table.point.Y;
         }
-        private void TB_TextChanged(object sender, EventArgs e)
-        {
-            TextBox tx = (TextBox)sender;
-            int x;
-            if (TableCount > 0)
-                x = TableCount - 1;
-            else
-                x = TableCount;
-            Table[x].DataBase[int.Parse(tx.Name), tx.TabIndex] = tx.Text;
-        }
-        public void saveToFile(string filename,int tableIndex =0)
+        public void saveToFile(string filename)
         {
             System.IO.FileStream fs = System.IO.File.Open(filename, System.IO.FileMode.OpenOrCreate);
-            string x = Table[tableIndex].height + ":" + Table[tableIndex].width + "/" ;
-            for (int i = 0; i <= Table[tableIndex].height ; i++)
+            string x = Table.height + ":" + Table.width + "/" ;
+            for (int i = 0; i <= Table.height ; i++)
             {
-                for (int j = 0; j <= Table[tableIndex].width ; j++)
+                for (int j = 0; j <= Table.width ; j++)
                 {
                     if (i >=0  && j >= 1)
                     {
-                        string u = Table[tableIndex].DataBase[i, j];
+                        string u = Table.DataBase[i, j];
                         if (u == null) u = "";
                             x += u.Length + ":" + u;
                     }
@@ -292,12 +271,9 @@ namespace WindowsFormsApp4
             
 
         }
-        private void addData(string input,int x,int y,bool enabl = true ,int tableIndex =0)
+        private void addData(string input,int x,int y,bool enabl = true )
         {
-            Table[tableIndex].DataBase[x, y] = input;
-            TextBox tx = Table[tableIndex].TB[x, y].Tag as TextBox;
-            tx.Text = input;
-            tx.Enabled = enabl;
+            Table.DataBase[x, y] = input;
         }
     }
 }

c01cc54ea1b9b388d4a01c9ba51f40711d8ab36a
HomamAlamier <homamassr2@gmail.com>
2018-12-19 20:30:19 +0200

    Update Tables.cs
    Update v0.09:
    Added some useful get commands 😄 
    new changes!
    
    CHANGE LOG:
    1.You can now set a font for every cell and also set a color to it
    2.added cell indector to interact with the table with arrows keys!
    3.bug fix's "As always Bug's 😃  "
    
    and still in development !
    i will improve table render and database interact and bug fix's 😠 
    
    Best Wish's 😄

--- Tables.cs
+++ Tables.cs
@@ -5,123 +5,190 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
-namespace WindowsFormsApp4
-{ 
-    class Tables
+class Tables
+{
+    public int WindowsMode_FindAndReplace = 10020;
+    public int WindowsMode_FindOnly = 10300;
+    public struct mytextbox
     {
-        public int WindowsMode_FindAndReplace = 10020;
-        public int WindowsMode_FindOnly = 10300;
-        public struct mytextbox
+        public Point location;
+        public int width;
+        public int height;
+        public int allHeight;
+        public int allWidth;
+        public Color BackColor;
+        public Font TextFont;
+    }
+    public struct mystract
+    {
+        public string[,] DataBase;
+        public int height;
+        public int width;
+        public mytextbox[,] points;
+        public System.Drawing.Point point;
+        public int TableX;
+        public int TableY;
+        public Form InForm;
+        public int FoucsI;
+        public int FoucsJ;
+    }
+    public static bool IsCreated = false;
+    static bool clicked = false;
+    public static mystract Table;
+    public static int TableCount = 0;
+    public void CreateTable(int Height, int Width, Form frm, int x, int y)
+    {
+        if (IsCreated == false)
+            IsCreated = true;
+        else
+            return;
+        Table.DataBase = new string[Height + 1, Width + 1];
+        Table.height = Height;
+        Table.width = Width;
+        Table.point = new System.Drawing.Point(x, y);
+        Table.InForm = frm;
+        Table.TableX = x;
+        Table.TableY = y;
+    }
+    public void DrawTable(int width, int height, bool redraw = false)
+    {
+        if (IsCreated == false) return;
+        int x = Table.TableX;
+        int y = Table.TableY;
+        if (redraw == false) Table.points = new mytextbox[Table.height + 1, Table.width + 1];
+        int w = (width / (Table.width));
+        int h = 30;
+        int hh = h;
+        h = h * Table.height;
+        int wr = (width % (Table.width));
+        Graphics g;
+        g = Table.InForm.CreateGraphics();
+        g.Clear(Color.White);
+        Pen myPen = new Pen(Color.Black);
+        myPen.Width = 1;
+        int x2 = 0, y2;
+        int cWidth = w * Table.width;
+        for (int i = 0; i <= Table.height; i++)
         {
-            public Point location;
-            public int width;
-            public int height;
-            public int allHeight;
-            public int allWidth;
-        }
-        public struct mystract
-        {
-           public string[,] DataBase;
-            public int height;
-            public int width;
-            public mytextbox[,] points;
-            public System.Drawing.Point point;
-            public int TableX;
-            public int TableY;
-            public Form1 InForm;
-            
-        }
-        public static  bool IsCreated = false;
-        static bool clicked = false;
-       public static mystract Table ;
-        public static int TableCount = 0;
-        public void CreateTable(int Height,int Width,Form1 frm,int x , int y )
-        {
-            if (IsCreated == false)
-                IsCreated = true;
-            else
-                return;
-            Table.DataBase = new string[Height + 1, Width +1];
-            Table.height = Height;
-            Table.width = Width;
-            Table.point = new System.Drawing.Point(x, y);
-            Table.InForm = frm;
-            Table.TableX = x;
-            Table.TableY = y;
-        }
-        public void DrawTable(int width,int height)
-        {
-            if (IsCreated == false) return;
-            int x = Table.TableX;
-            int y = Table.TableY;
-            Table.points = new mytextbox[ Table.height, Table.width];
-            int w = (width / (Table.width)) ;
-            int h = (height / (Table.height)) ;
-            int hh = h;
-            h = h * Table.height;
-            int wr = (width % (Table.width));
-            Graphics g;
-            g = Table.InForm.CreateGraphics();
-            g.Clear(Color.White);
-            Pen myPen = new Pen(Color.Black);
-            myPen.Width = 1;
-            g.DrawLine(myPen, x, y, x + width, y);
-            g.DrawLine(myPen, x, y, x , y+h);
-            g.DrawLine(myPen, x, y+h , x+width , y + h);
-            g.DrawLine(myPen, x + width, y, x + width, y + h);
-            int x2=0, y2;
-            for (int i = 0; i < Table.height; i++)
+            for (int j = 0; j <= Table.width; j++)
             {
-                for (int j = 0; j < Table.width; j++)
-                {
-                    Table.points[i, j].allWidth = width;
-                    Table.points[i, j].allHeight = height;
-                    //TextBox tx = Table.TB[0, j].Tag as TextBox;
-                    Table.points[i, j].location = new Point(x2, 0);
-                    x2 = w * j;
-                    g.DrawLine(myPen, x2, y, x2, y + h);
-                    Table.points[i, j].width = w;
-                    y2 = hh * i;
-                    y2 += y;
-                    g.DrawLine(myPen, x, y2, x + width, y2);
-                    Table.points[i,j].location = new Point(Table.points[i,j].location.X, y2);
-                    Table.points[i,j].height = hh;
-                }
+                if (redraw == false) Table.points[i, j].TextFont = new Font("Arial", 10);
+                Table.points[i, j].allWidth = width;
+                Table.points[i, j].allHeight = height;
+                //TextBox tx = Table.TB[0, j].Tag as TextBox;
+                Table.points[i, j].location = new Point(x2, 0);
+                x2 = w * j;
+                g.DrawLine(myPen, x2, y, x2, y + h);
+                Table.points[i, j].width = w;
+                y2 = hh * i;
+                y2 += y;
+                g.DrawLine(myPen, x, y2, cWidth, y2);
+                Table.points[i, j].location = new Point(Table.points[i, j].location.X, y2);
+                Table.points[i, j].height = hh;
             }
-            for (int i = 0; i < Table.height ; i++)
+        }
+        g.DrawLine(myPen, x, y, cWidth, y);
+        g.DrawLine(myPen, x, y, x, y + h);
+        g.DrawLine(myPen, x, y + h, cWidth, y + h);
+        for (int i = 0; i <= Table.height; i++)
+        {
+            for (int j = 0; j <= Table.width; j++)
             {
-                for (int j = 0; j < Table.width ; j++)
+
+                if (Table.DataBase[i, j] != null)
                 {
 
-                if (Table.DataBase[i,j] != null)
-                    {
-                        string tT= Table.DataBase[i, j];
-                        Font fnt = new Font("Arial",10);
-                        SolidBrush br = new SolidBrush(Color.Black) ;
-                        int max = Table.points[i, j].width / 10;
-                        if (tT.Length > max) tT=tT.Substring(0, max) + "...";
-                        g.DrawString(tT, fnt, br,new Point(Table.points[i, j].location.X , (Table.points[i, j].location.Y- Table.points[i, j].height) + 2));
-                    }
+                    SolidBrush br = new SolidBrush(Color.Black);
+                    string tT = Table.DataBase[i, j];
+                    int z = Convert.ToInt32(Table.points[i, j].TextFont.Size);
+                    z -= 3;
+                    int max = Table.points[i, j].width / z;
+                    if (max > 3 && tT.Length > max) tT = tT.Substring(0, max - 3) + "...";
+                    g.DrawString(tT, Table.points[i, j].TextFont, br, new Point(Table.points[i, j].location.X, (Table.points[i, j].location.Y - Table.points[i, j].height) + 2));
                 }
             }
         }
+        for (int i = 0; i <= Table.height; i++)
+        {
+            for (int j = 0; j <= Table.width; j++)
+            {
+
+                Color c = Color.FromArgb(0, 0, 0, 0);
+                if (Table.points[i, j].BackColor.IsEmpty == true)
+                    Table.points[i, j].BackColor = Color.White;
+                else
+                    SetCellColor(Table.points[i, j].BackColor, i, j);
+            }
+        }
+        if (redraw == false)
+        {
+            Table.FoucsI = 0;
+            Table.FoucsJ = 1;
+        }
+        int xR = Table.points[Table.FoucsI, Table.FoucsJ].location.X;
+        int yR = Table.points[Table.FoucsI, Table.FoucsJ].location.Y;
+        int xR2 = Table.points[Table.FoucsI, Table.FoucsJ].width;
+        int yR2 = Table.points[Table.FoucsI, Table.FoucsJ].height - 10;
+        Rectangle rec = new Rectangle(xR, yR, xR2, yR2 + 10);
+        g = Table.InForm.CreateGraphics();
+        int R, G, B;
+        if (Table.points[Table.FoucsI, Table.FoucsJ].BackColor.R > 10)
+            R = Table.points[Table.FoucsI, Table.FoucsJ].BackColor.R - 10;
+        else
+            R = Table.points[Table.FoucsI, Table.FoucsJ].BackColor.R+10;
+        if (Table.points[Table.FoucsI, Table.FoucsJ].BackColor.G > 10)
+            G = Table.points[Table.FoucsI, Table.FoucsJ].BackColor.G - 10;
+        else
+            G = Table.points[Table.FoucsI, Table.FoucsJ].BackColor.G+10;
+        if (Table.points[Table.FoucsI, Table.FoucsJ].BackColor.B > 10)
+            B = Table.points[Table.FoucsI, Table.FoucsJ].BackColor.B - 10;
+        else
+            B = Table.points[Table.FoucsI, Table.FoucsJ].BackColor.B+10;
+        Pen pn = new Pen(Color.FromArgb(255, R, G, B));
+        pn.Width = 2;
+        g.DrawRectangle(pn, rec);
+    }
+    public Point GetSelectedCell()
+    {
+        if (IsCreated == false) return new Point(0, 0);
+        int i, j;
+        i = Table.FoucsI;
+        j = Table.FoucsJ;
+        return new Point(i, j);
+    }
+    public void selectCell(int i, int j)
+    {
+        if (IsCreated == false) return;
+        Table.FoucsI = i;
+        Table.FoucsJ = j;
+        DrawTable(Table.points[0, 0].allWidth, Table.points[0, 0].allHeight, true);
+    }
+    public Point GetCellAxis(int i,int j)
+    {
+        return Table.points[i, j].location;
+    }
+    public Point GetTableAxis()
+    {
+        return new Point(Table.width, Table.height);
+    }
         public void ClickOnTable(int x,int y)
         {
             if (IsCreated == false) return;
             if (clicked==false )
             {
-                for (int i = 0; i < Table.height ; i++)
+                for (int i = 0; i <= Table.height ; i++)
                 {
-                    for (int j = 1; j < Table.width; j++)
+                    for (int j = 1; j <= Table.width; j++)
                     {
-                        if (Table.points[i,j].location.X <= x && (Table.points[i,j].location.X + Table.points[i,j].width) >= x && (Table.points[i,j].location.Y) >= y)
+                        if (Table.points[i,j].location.X < x && (Table.points[i,j].location.X + Table.points[i,j].width) > x && (Table.points[i,j].location.Y) > y)
                         {
                            //params
                             TextBox tx = new TextBox();
+                        tx.MinimumSize = new Size(Table.points[i, j].width, Table.points[i, j].height + 1);
                             tx.Height = Table.points[i,j].height + 1;
                             tx.Width = Table.points[i,j].width;
-                            tx.Location = new Point(Table.points[i, j].location.X + 1, Table.points[i, j].location.Y - Table.points[i, j].height);
-                            tx.Multiline = true;
+                        tx.Location = new Point(Table.points[i, j].location.X + 1, Table.points[i, j].location.Y - Table.points[i, j].height);
+                            //tx.Multiline = true;
                             tx.Leave += new EventHandler(textVlick_Leave);
                             tx.KeyPress += new KeyPressEventHandler(textClick_KeyPress);
                             tx.TextChanged += new EventHandler(textClick_TextChanged);
@@ -129,10 +196,12 @@ namespace WindowsFormsApp4
                             tx.Name = i.ToString();
                             tx.BackColor = Color.LawnGreen;
                             tx.Text = Table.DataBase[i, j];
+                        tx.Font = Table.points[i, j].TextFont;
                             // add
                             Table.InForm.Controls.Add(tx); 
                             tx.TextAlign = HorizontalAlignment.Center;
                             tx.Focus();
+                        clicked = true;
                             return;
                         }
                     }
@@ -149,7 +218,8 @@ namespace WindowsFormsApp4
         {
             TextBox tx = (TextBox)sender;
             tx.Dispose();
-            DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight);
+            DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight,true);
+        clicked = false;
         }
         private void textClick_KeyPress(object sender,KeyPressEventArgs e)
         {
@@ -157,8 +227,9 @@ namespace WindowsFormsApp4
             {
                 TextBox tx = (TextBox)sender;
                 tx.Dispose();
-                DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight);
-            }
+                DrawTable( Table.points[0, 1].allWidth, Table.points[0, 1].allHeight,true);
+            clicked = false;
+        }
             }
 
         public void ResizeColomun(int newWidth,int colNum)
@@ -186,19 +257,61 @@ namespace WindowsFormsApp4
                 {
                     if (Table.DataBase[i, j] != null && Table.DataBase[i,j].IndexOf(input) >=0 )
                     {
-                        Graphics g = Table.InForm.CreateGraphics();
-                        int x = Table.points[i, j].location.X + 1;
-                        int y = Table.points[i, j].location.Y - Table.points[i, j].height;
-                        y++;
-                        Rectangle rec = new Rectangle(new Point(x,y), new Size(Table.points[i,j].width-1, Table.points[i,j].height-1));
-                        Region r = new Region(rec);
-                        g.FillRegion(new SolidBrush(Color.LightCyan), r);
-                        g.DrawString(Table.DataBase[i,j], new Font("Arial", 10), new SolidBrush(Color.Black), new Point(x,y));
+                    SetCellColor(Color.LightCyan, i, j);
                     }
                 }
             }
         }
-        public void findAndreplace(string input,string input2)
+    private void SetCellColor(Color col,int i,int j)
+    {
+        if (IsCreated == false) return;
+        Table.points[i, j].BackColor = col;
+        Graphics g = Table.InForm.CreateGraphics();
+        int x = Table.points[i, j].location.X + 1;
+        int y = Table.points[i, j].location.Y - Table.points[i, j].height;
+        y++;
+        Rectangle rec = new Rectangle(new Point(x, y), new Size(Table.points[i, j].width - 1, Table.points[i, j].height - 1));
+        Region r = new Region(rec);
+        g.FillRegion(new SolidBrush(col), r);
+        string tT = Table.DataBase[i, j];
+        int z = Convert.ToInt32(Table.points[i, j].TextFont.Size);
+        z-=3;
+        int max = Table.points[i, j].width / z;
+        if (max > 3 && tT != null && tT.Length > max) tT = tT.Substring(0, max - 3) + "...";
+        g.DrawString(tT, Table.points[i,j].TextFont, new SolidBrush(Color.Black), new Point(x, y));
+    }
+    public void ColorCell(Color col,int x , int y)
+    {
+        if (IsCreated == false) return;
+        int i = 0, j = 0;
+        for (i = 0; i < Table.height; i++)
+        {
+            for (j = 0; j < Table.width; j++)
+            {
+                if (Table.points[i,j].location.X <= x && (Table.points[i, j].location.Y - Table.points[i, j].height ) <=y && x <= (Table.points[i, j].location.X+ Table.points[i, j].width))
+                {
+                    SetCellColor(Color.Red, i, j);
+                }
+            }
+        }
+    }
+    public void FontCell(Font fnt, int x, int y)
+    {
+        if (IsCreated == false) return;
+        int i = 0, j = 0;
+        for (i = 0; i < Table.height; i++)
+        {
+            for (j = 0; j < Table.width; j++)
+            {
+                if (Table.points[i, j].location.X <= x && (Table.points[i, j].location.Y - Table.points[i, j].height) <= y && x <= (Table.points[i, j].location.X + Table.points[i, j].width))
+                {
+                    Table.points[i, j].TextFont = fnt;
+                }
+            }
+        }
+        DrawTable(Table.points[0, 0].allWidth, Table.points[0, 0].allHeight, true);
+    }
+    public void findAndreplace(string input,string input2)
         {
             if (IsCreated == false) return;
             int i = 0, j = 0;
@@ -212,7 +325,7 @@ namespace WindowsFormsApp4
                     }
                 }
             }
-            DrawTable(Table.points[0, 0].allWidth, Table.points[0, 0].allHeight);
+            DrawTable(Table.points[0, 0].allWidth, Table.points[0, 0].allHeight,true);
         }
         public int GetHeight()
         {
@@ -247,7 +360,7 @@ namespace WindowsFormsApp4
             fs.Write(info, 0, info.Length);
             fs.Close();
         }
-        public void openFormFile(string filename,Form1 frm)
+        public void openFormFile(string filename,Form frm)
         {
             System.IO.FileStream fs = System.IO.File.Open(filename, System.IO.FileMode.OpenOrCreate);
             byte[] b = new byte[1024];
@@ -354,4 +467,4 @@ namespace WindowsFormsApp4
                 findAndreplace(textBox.Text, text2.Text);
         }  
     }
-}
+
